69f90a00b9259fb87036ea7210364efcd8f11f40
SebasTZ <sebastz3120@gmail.com>
2025-02-25 22:49:32 -0500

    Mejorara de contexto

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,9 @@ export default async function Home() {
     <>
       <Hero />
       <main className="flex-1 flex flex-col gap-6 px-4">
-        <h1 className="font-medium text-xl mb-4">Integrado para gestionar tu Chatbot en localhost usando LM Studio</h1>
+        <h1 className="font-medium text-xl mb-4 text-center">
+          Entranado para responder a preguntas legales
+        </h1>
       </main>
     </>
   );
